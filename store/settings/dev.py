0be355300676737118b6ff8214f0a4f5165b09fd
Zhakya Nuristany <89075691+nuristany@users.noreply.github.com>
2024-03-29 12:26:25 +0430

    updated some files

--- store/settings/dev.py
+++ store/settings/dev.py
@@ -8,14 +8,27 @@ SECRET_KEY = 'django-insecure-b5^iox7!r&^k4bavbr2k4ukg2ot3*fo6)==!3wygr%r_f0^(n9
 DEBUG = True
 
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'reusestore',
-        'Host':'localhost',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'reusestore',
         'USER': 'root',
         'PASSWORD': 'root',
-
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME':'reusestore',
+#         'Host':'localhost',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+
+#     }
+# }
